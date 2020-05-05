716e7031696856f01bb97f0e1954a8a2f1af1cac
225junior <225juniorbrindou@gmail.com>
2020-05-05 08:26:20 +0000

    install pip 20.1 and django==2.2.10

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\exo_nan-3.20\django\django-tutoriel-first-project\django-tutoriel-first-project\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
